d3bec18c38974e733ddc6b3471a5e3b41a8d7028
Shon Arun <shonarun3785@gmail.com>
2026-04-18 12:07:53 +0530

    perf(functions): use size_t instead of unsigned - Changed 'unsigned' type to size_t. Addresses #3

--- Module_3_CoreConcepts/Functions/q107_array_is_sorted.c
+++ Module_3_CoreConcepts/Functions/q107_array_is_sorted.c
@@ -2,11 +2,11 @@
 
 #include <stdio.h>
 
-int array_is_sorted(int arr[], int size);
+int array_is_sorted(int arr[], size_t size);
 
 int main(void) {
     int arr[] = {1, 5, 6, 8, 9};
-    int size = sizeof(arr) / sizeof(arr[0]);
+    size_t size = sizeof(arr) / sizeof(arr[0]);
     int flag = array_is_sorted(arr, size);
     if (flag) {
         printf("Array is sorted.\n");
@@ -16,9 +16,9 @@ int main(void) {
     return 0;
 }
 
-int array_is_sorted(int arr[], int size) {
-    for (int i = 0; i < size - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
+int array_is_sorted(int arr[], size_t size) {
+    for (size_t i = 1; i < size - 1; i++) {
+        if (arr[i - 1] > arr[i]) {
             return 0;
         }
     }
